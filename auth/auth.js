2595934d61460f98c50f26c6713ebef699e8fb0f
HMH <musicbyhmh@gmail.com>
2023-04-17 14:24:46 +0200

    crud auth

--- auth/auth.js
+++ auth/auth.js
@@ -1,6 +1,7 @@
 const User = require('../model/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const jwtsecret = 'jBQBHSDHJBVHKBQFHUDWSHVGCDCBCHJBWHJCBSHG';
 //CRUD
 
 exports.register = async (req, res, next) => {
@@ -17,9 +18,20 @@ exports.register = async (req, res, next) => {
         })
         .then((User) => {
             const maxAge = 3*60*60;
-
+            const token = jwt.signing({id: User._id, username, role: User.role}, jwtsecret, {expiresIn: maxAge});
+            res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge*1000});
+            res.status(201).json({
+                message: 'Utilisateur créé avec succès.',
+                user: User._id, role: User.role,
+            });
+        })
+        .catch((error) => {
+            res.status(400).json({
+                message: 'L\'utilisateur existe déjà.',
+                error: error.message,
+            })
         });
-    })
+    });
 };
 exports.login = async (req, res, next) => {};
 exports.update = async (req, res, next) => {};
