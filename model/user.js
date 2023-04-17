5cd530c88903ac909b21b6673120060b9e622633
HMH <musicbyhmh@gmail.com>
2023-04-17 13:49:10 +0200

    maj

---
+++ model/user.js
@@ -0,0 +1,10 @@
+const Mongoose = require('mongoose');
+const UserSchema = Mongoose.Schema({
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true, minlength: 6},
+    role: {type: String, required: true, default: 'user'}
+});
+
+const User = Mongoose.model('User', UserSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const connectDB = require('./db');
+const { adminAuth, userAuth } = require('./middleware/auth');
 
 connectDB();
 
@@ -9,6 +10,23 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(cookieParser());
 
+//route principale
+app.get('/', (req, res) => res.render('home'));
+app.get('/register', (req, res) => res.render('register'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/logout', (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
+});
+
+app.get('/admin', adminAuth, (req, res) => res.render('admin'));
+app.get('/user', userAuth, (req, res) => res.render('user'));
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
 });
