98500cb694334d3036d11a85f4694acac11d3f3d
HMH <musicbyhmh@gmail.com>
2023-04-17 13:21:58 +0200

    réglage server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const connectDB = require('./db');
 
 connectDB();
 
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
